5bf7df3e0cfdb6193332c97b2e78aedfa2d51a4a
BobisBilly <126552989+BobisBilly@users.noreply.github.com>
2023-11-22 19:26:04 +1300

    Update value.php

--- value.php
+++ value.php
@@ -1,1 +1,12 @@
-header
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+
+$data = '[
+{"data1": "value1"},
+{"data2": "value2"}
+]'
+
+echo $data;
+?>
