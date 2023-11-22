0f4f6a8b31640bd51998cc58c8c451f4d7365019
BobisBilly <126552989+BobisBilly@users.noreply.github.com>
2023-11-22 19:39:42 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 const valueButton = document.querySelector('#valueButton');
+
+valueButton.onClick(makePost())
+
+function makePost() {
+  var postData = new FormData();
+  postData.append('data', "success");
+  fetch("https://bobisbilly.github.io/SideProject", {
+    method: "POST",
+    body: postData,
+  })
+  .then(response => response.json())
+  .then(json => {
+    let row = `<tr><th>Data</th><th>Value</th></tr>`;
+    json.forEach(data =>  {
+      row += `<tr><td>${data.data}
+          </td><td>${data.value}</td></tr>`;
+    });
+    const row = document.getElementById('tableone');
+  });
+}
